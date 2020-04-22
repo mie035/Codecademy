b1a259aa9950727be01d13811da79651820f904a
myano <lptakes276@icloud.com>
2020-04-23 02:31:36 +0900

    finished quiz

--- DataStructure/data_structure/Towers_of_Hanoi/main.py
+++ DataStructure/data_structure/Towers_of_Hanoi/main.py
@@ -25,5 +25,46 @@ num_optimal_moves = 2 ** num_disks -1
 print("\nThe fastest you can solve this game is in {0} moves".format(num_optimal_moves))
 #Get User Input
 
-
+def get_input():
+  choices = []
+  for stack in stacks:
+    name = stack.get_name()
+    choices.append(name[0])
+  while True:
+    for i in range(len(stacks)):
+      name = stacks[i].get_name()
+      letter = choices[i]
+      print("Enter {0} for {1}".format(letter,name))
+    user_input = input()
+    if user_input in choices:
+      for i in range(len(stacks)):
+        if user_input == choices[i]:
+          return stacks[i]
 #Play the Game
+num_user_moves = 0
+while (right_stack.get_size() != num_disks):
+  print("\n\n\n...Current Stacks...")
+  for stack in stacks:
+    stack.print_items()
+  while True:
+    print("\nWhich stack do you want to move from?\n")
+    from_stack = get_input()
+    print("\nWhich stack do you want to move to?\n")
+    to_stack = get_input()
+    if from_stack.get_size() == 0:
+      print("\n\nInvalid Move. Try Again")
+      continue
+    elif (to_stack.get_size() ==0) or from_stack.peek()<to_stack.peek():
+      disk = from_stack.pop()
+      to_stack.push(disk)
+      num_user_moves += 1
+      print("you let {0}:{1} moved to {2}".format(from_stack.get_name(),disk,to_stack.get_name()))
+    else:
+      print("\n\nInvalid Move. Try Again")
+      break
+    else:
+      continue
+    break
+    
+  print("\n\nYou completed the game in {0} moves, and the optimal number of moves is {1}".format(num_user_moves, num_optimal_moves))
+
