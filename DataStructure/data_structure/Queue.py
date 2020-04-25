b3d8c993b55070dbeed6b597a4dc69cf3b863ada
myano <lptakes276@icloud.com>
2020-04-25 18:42:48 +0900

    finished 5.Queues:Python

--- DataStructure/data_structure/Queue.py
+++ DataStructure/data_structure/Queue.py
@@ -1,31 +1,45 @@
 from node import Node
 
 class Queue:
-  def __init__(self, head = None, tail = None, max_size=None):
-    self.head = head
-    self.tail = tail
+  def __init__(self, max_size=None):
+    self.head = None
+    self.tail = None
     self.max_size = max_size
     self.size = 0
     
-  # Add your enqueue method below:
-  def enqueue(self,value):
+  def enqueue(self, value):
     if self.has_space():
       item_to_add = Node(value)
-      print("Adding " + str(item_to_add.get_value()+" to the queue!"))
-      if self.get_size()==0:
+      print("Adding " + str(item_to_add.get_value()) + " to the queue!")
+      if self.is_empty():
         self.head = item_to_add
         self.tail = item_to_add
       else:
         self.tail.set_next_node(item_to_add)
         self.tail = item_to_add
-        self.size += 1
+      self.size += 1
     else:
-      print("cannot enqueue!")
+      print("Sorry, no more room!")
+         
+  def dequeue(self):
+    if self.get_size() > 0:
+      item_to_remove = self.head
+      print(str(item_to_remove.get_value()) + " is served!")
+      if self.get_size() == 1:
+        self.head = None
+        self.tail = None
+      else:
+        self.head = self.head.get_next_node()
+      self.size -= 1
+      return item_to_remove.get_value()
+    else:
+      print("The queue is totally empty!")
+  
   def peek(self):
-    if self.is_empty():
-      print("Nothing to see here!")
-    else:
+    if self.size > 0:
       return self.head.get_value()
+    else:
+      print("No orders waiting!")
   
   def get_size(self):
     return self.size
@@ -38,6 +52,3 @@ class Queue:
     
   def is_empty(self):
     return self.size == 0
-
-q = Queue()
-q.enqueue("all the fluffy kitties")
